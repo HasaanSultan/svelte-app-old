6b51bf761b576da454ef7ef0d04bde05ccc0c922
Hasaan Sultan <Hasaan.sultan@visionx.io>
2022-11-18 11:29:54 +0500

    initial setup with bubbles ui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+	import 'bubbles-ui/css/app.css';
+	import { Header, Row, Column100, Card, Form, CardHeader } from 'bubbles-ui';
+	$: name = 'Hasaan';
+
+	$: inputs = [
+		{
+			type: 'text',
+			id: 'name',
+			label: 'name',
+			validation: 'string|required',
+			value: name,
+			error: 'Name is required'
+		},
+		{
+			label: 'Location Address',
+			id: 'address',
+			type: 'textarea',
+			validation: 'string|required'
+		},
+		{
+			type: 'switch',
+			id: 'preferences.email',
+			label: 'Email Updates',
+			value: true,
+			desc: 'Let us know if you want to receive email updates',
+			disabled: false,
+			error: 'An error occurred',
+			validation: 'accepted' //best UX is to not make switches mandatory, use a checkbox instead if you need
+		},
+
+		{
+			type: 'submit',
+			label: 'Add',
+			onsubmit: async (event) => {
+				const data = await getFormData(inputs);
+				console.log(data);
+			}
+		}
+	];
+</script>
+
+<Header
+	title="New Location"
+	breadcrumbs={[
+		{ label: 'Locations', href: '/' },
+		{ label: 'New', href: '/locations/new' }
+	]}
+/>
+
+<Header
+	title="PackageX Cloud"
+	breadcrumbs={false}
+	buttons={[
+		{
+			icon: 'more',
+			color: 'gray-lighter',
+			search: false,
+			options: [
+				{
+					label: 'Organization',
+					caption: 'Update company settings',
+					// href: '/dashboard/organization',
+					hidden: false
+				}
+			]
+		}
+	]}
+/>
+<Row>
+	<Column100>
+		<Card>
+			<CardHeader title="Add Location" />
+			<Form {inputs} />
+		</Card>
+	</Column100>
+</Row>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import vercel from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: vercel({
+			edge: true
+		})
+	}
+};
+
+export default config;
